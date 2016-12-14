c92181a997e38ecbe96cc3f09d0ff545f5a47004
Ryan <ryan2002.rs@gmail.com>
2016-12-14 12:11:39 +1100

    Setup and bg

---
+++ src/chess/Main.java
@@ -0,0 +1,25 @@
+package chess;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.SlickException;
+
+public class Main {
+public static void main(String[] a){
+	try
+	{
+		AppGameContainer appgc;
+		appgc = new AppGameContainer(new ChessGame("Chess"));
+		appgc.setDisplayMode(705, 720, false);
+		appgc.start();
+		
+	}
+	catch (SlickException ex)
+	{
+		Logger.getLogger(ChessGame.class.getName()).log(Level.SEVERE, null, ex);
+	}
+}
+}
+
